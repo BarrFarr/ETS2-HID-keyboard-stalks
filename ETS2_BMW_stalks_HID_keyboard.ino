7350936aa1b80f57fca8c4919f5154a8ebd5a10c
BarrFarr <140327049+BarrFarr@users.noreply.github.com>
2025-09-21 13:21:21 +0200

    Update ETS2_BMW_stalks_HID_keyboard.ino

--- ETS2_BMW_stalks_HID_keyboard.ino
+++ ETS2_BMW_stalks_HID_keyboard.ino
@@ -116,3 +116,116 @@ void loop() {
   }
 
   // === DIGITAL BUTTONS SECTION (RUNS CONSTANTLY) ===
+
+  bool current_pin2_state = (digitalRead(pin2) == LOW);
+  bool current_pin3_state = (digitalRead(pin3) == LOW);
+  bool current_pin4_state = (digitalRead(pin4) == LOW);
+  bool current_pin5_state = (digitalRead(pin5) == LOW);
+  bool current_pin6_state = (digitalRead(pin6) == LOW);
+  bool current_pin7_state = (digitalRead(pin7) == LOW);
+  bool current_pin8_state = (digitalRead(pin8) == LOW);
+  bool current_pin9_state = (digitalRead(pin9) == LOW);
+  bool current_pin10_state = (digitalRead(pin10) == LOW);
+  bool current_pin16_state = (digitalRead(pin16) == LOW);
+  bool current_pin21_state = (digitalRead(pin21) == LOW);
+
+  // Rest of the code
+  if (current_pin9_state && current_pin6_state) {
+    if (!combo_9_6_pressed) {
+      Keyboard.press(KEY_KP_5);
+      combo_9_6_pressed = true;
+      ledState = !ledState;
+      digitalWrite(ledPin5, ledState ? HIGH : LOW);
+    }
+  } else {
+    if (combo_9_6_pressed) {
+      Keyboard.release(KEY_KP_5);
+      combo_9_6_pressed = false;
+    }
+    if (current_pin6_state && !current_pin9_state && pin9_state) {
+      Keyboard.press(KEY_KP_8);
+      digitalWrite(ledPin8, HIGH);
+    } else if (pin6_state && !current_pin6_state) {
+      Keyboard.release(KEY_KP_8);
+      digitalWrite(ledPin8, LOW);
+    }
+  }
+
+  if (current_pin9_state && current_pin21_state) {
+    if (!combo_9_21_pressed) {
+      Keyboard.press(KEY_KP_2);
+      digitalWrite(ledPin2, HIGH);
+      combo_9_21_pressed = true;
+    }
+  } else {
+    if (combo_9_21_pressed) {
+      Keyboard.release(KEY_KP_2);
+      digitalWrite(ledPin2, LOW);
+      combo_9_21_pressed = false;
+    }
+    if (current_pin21_state && !current_pin9_state && pin9_state) {
+      Keyboard.press(KEY_KP_0);
+    } else if (pin21_state && !current_pin21_state) {
+      Keyboard.release(KEY_KP_0);
+    }
+  }
+
+  if (current_pin2_state && current_pin3_state) {
+    if (!combo_2_3_pressed) {
+      Keyboard.press('.');
+      combo_2_3_pressed = true;
+    }
+  } else {
+    if (combo_2_3_pressed) {
+      Keyboard.release('.');
+      combo_2_3_pressed = false;
+    }
+    if (current_pin2_state && !current_pin3_state && pin3_state) {
+      Keyboard.press('/');
+    } else if (pin2_state && !current_pin2_state) {
+      Keyboard.release('/');
+    }
+  }
+
+  if (current_pin3_state && !current_pin2_state && !pin3_state) { Keyboard.press(','); pin3_state = true; } 
+  else if (!current_pin3_state && pin3_state) { Keyboard.release(','); pin3_state = false; }
+  
+  if (current_pin5_state && !pin5_state) { Keyboard.press(';'); pin5_state = true; } 
+  else if (!current_pin5_state && pin5_state) { Keyboard.release(';'); pin5_state = false; }
+  
+  if (current_pin4_state && !pin4_state) {
+    Keyboard.press('\'');
+    semicolonLedState = !semicolonLedState;
+    digitalWrite(ledPinSemicolon, semicolonLedState ? HIGH : LOW);
+    pin4_state = true;
+  } else if (!current_pin4_state && pin4_state) {
+    Keyboard.release('\'');
+    pin4_state = false;
+  }
+  
+  if (current_pin16_state && !pin16_state) { Keyboard.press(KEY_KP_1); pin16_state = true; } 
+  else if (!current_pin16_state && pin16_state) { Keyboard.release(KEY_KP_1); pin16_state = false; }
+  
+  if (current_pin10_state && !pin10_state) { Keyboard.press(KEY_KP_7); pin10_state = true; } 
+  else if (!current_pin10_state && pin10_state) { Keyboard.release(KEY_KP_7); pin10_state = false; }
+
+  if (current_pin7_state && !pin7_state) { Keyboard.press(KEY_KP_3); pin7_state = true; } 
+  else if (!current_pin7_state && pin7_state) { Keyboard.release(KEY_KP_3); pin7_state = false; }
+  
+  if (current_pin8_state && !pin8_state) { Keyboard.press(KEY_KP_9); pin8_state = true; } 
+  else if (!current_pin8_state && pin8_state) { Keyboard.release(KEY_KP_9); pin8_state = false; }
+
+  pin2_state = current_pin2_state;
+  pin3_state = current_pin3_state;
+  pin4_state = current_pin4_state;
+  pin5_state = current_pin5_state;
+  pin6_state = current_pin6_state;
+  pin7_state = current_pin7_state;
+  pin8_state = current_pin8_state;
+  pin9_state = current_pin9_state;
+  pin10_state = current_pin10_state;
+  pin16_state = current_pin16_state;
+  pin21_state = current_pin21_state;
+
+  delay(10);
+}
